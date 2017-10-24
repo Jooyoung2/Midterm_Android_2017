ddc608bb068879ccee6604debf15ff9953b2117e
Xtreme <appcla352@naver.com>
2017-10-25 03:34:24 +0900

    added bottomNavigationView

---
+++ app/src/main/java/com/jooyoung/android_mid_2017/MainActivity.java
@@ -0,0 +1,18 @@
+package com.jooyoung.android_mid_2017;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+
+}
